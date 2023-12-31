c749024ab2f7c5a8e89a7c0fdd6112066cf4fee6
Seungjun Kim <zzjoon97@naver.com>
2023-12-31 19:22:56 +0900

    "231231"

---
+++ src/scanner/ex/ScannerWhileEx3.java
@@ -0,0 +1,34 @@
+package scanner.ex;
+
+import java.util.Scanner;
+
+public class ScannerWhileEx3 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int sum = 0;
+        int count = 0;
+        int input2 = 0;
+
+        System.out.println("숫자를 입력하세요. 입력을 중단하려면 -1을 입력하세요:");
+        while (true) {
+
+            int number = input.nextInt();
+            if (number == -1) {
+                break;
+            }
+            sum += number;
+            count += 1;
+
+        }
+
+
+//        while ((input2 = input.nextInt()) != -1){
+//            sum += input2;
+//            count++;
+//        }
+        double average = (double) sum / count;
+        System.out.println("입력한 숫자들의 합계: " + sum);
+        System.out.println("입력한 숫자들의 평균: " + average);
+    }
+}
